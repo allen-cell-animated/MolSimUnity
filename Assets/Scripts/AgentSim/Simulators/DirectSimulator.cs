c8d00ae451b26fa62dd5c2650636adea947cd07c
Blair Lyons <blair208@gmail.com>
2018-01-25 16:41:37 -0800

    DirectSimulator does random motion without physics, PhysicalDirectSimulator uses physics

--- Assets/Scripts/AgentSim/Simulators/DirectSimulator.cs
+++ Assets/Scripts/AgentSim/Simulators/DirectSimulator.cs
@@ -9,42 +9,22 @@ namespace AICS.AgentSim
 		[Tooltip( "([Agent's scale] meters)^2 / s" )]
 		[SerializeField] protected float diffusionCoefficient;
 
-		protected Collider theCollider;
-		protected Rigidbody body;
-
-		float forceMagnitude
+		protected float displacement
 		{
-			get {
-				float meanForce = Time.deltaTime * 5E6f * Mathf.Sqrt( diffusionCoefficient * agent.scale );
-				return Mathf.Log( Random.Range( float.Epsilon, 1f ) ) / (-1f / meanForce);
+			get 
+			{
+				return Time.deltaTime * Mathf.Sqrt( diffusionCoefficient * agent.scale );
 			}
-		}
-
-		float torqueMagnitude
-		{
-			get {
-				float meanForce = Time.deltaTime * 5E6f * Mathf.Sqrt( diffusionCoefficient * agent.scale );
-				return Mathf.Log( Random.Range( float.Epsilon, 1f ) ) / (-1f / meanForce);
-			}
-		}
-
-		void Start ()
-		{
-			AddRigidbodyCollider();
-		}
-
-		protected virtual void AddRigidbodyCollider ()
-		{
-			theCollider = gameObject.AddComponent<SphereCollider>();
-			body = gameObject.AddComponent<Rigidbody>();
-			body.useGravity = false;
 		}
 
 		public override void SimulateTo (float time)
 		{
-			body.velocity = body.angularVelocity = Vector3.zero;
-			body.AddForce( forceMagnitude * Random.onUnitSphere );
-			body.AddTorque( torqueMagnitude * Random.onUnitSphere );
+			DoRandomStep();
+		}
+
+		protected virtual void DoRandomStep ()
+		{
+			transform.position += 2E3f * displacement * Random.onUnitSphere;
 		}
 	}
 }
---
+++ Assets/Scripts/AgentSim/Simulators/PhysicalDirectSimulator.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AICS.AgentSim
+{
+	public class PhysicalDirectSimulator : DirectSimulator 
+	{
+		protected Collider theCollider;
+		protected Rigidbody body;
+
+		float forceMagnitude
+		{
+			get 
+			{
+				float meanForce = 5E6f * displacement;
+				return Mathf.Log( Random.Range( float.Epsilon, 1f ) ) / (-1f / meanForce);
+			}
+		}
+
+		float torqueMagnitude
+		{
+			get 
+			{
+				float meanForce = 5E6f * displacement;
+				return Mathf.Log( Random.Range( float.Epsilon, 1f ) ) / (-1f / meanForce);
+			}
+		}
+
+		void Start ()
+		{
+			AddRigidbodyCollider();
+		}
+
+		void AddRigidbodyCollider ()
+		{
+			theCollider = gameObject.AddComponent<SphereCollider>();
+			body = gameObject.AddComponent<Rigidbody>();
+			body.drag = 10f;
+			body.useGravity = false;
+		}
+
+		protected override void DoRandomStep ()
+		{
+			body.velocity = body.angularVelocity = Vector3.zero;
+			body.AddForce( forceMagnitude * Random.onUnitSphere );
+			body.AddTorque( torqueMagnitude * Random.onUnitSphere );
+		}
+	}
+}
