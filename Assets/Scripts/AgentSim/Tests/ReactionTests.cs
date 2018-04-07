bec3ebaefc9951b06aa84b77ac0ed0eb62332a76
Blair Lyons <blair208@gmail.com>
2018-04-06 17:01:22 -0700

    test for multisite P passes

--- Assets/Scripts/AgentSim/Tests/ReactionTests.cs
+++ Assets/Scripts/AgentSim/Tests/ReactionTests.cs
@@ -109,35 +109,69 @@ public class ReactionTests : AgentSimTests
     [UnityTest]
     public IEnumerator MultiStateChangeInMultiEnzymeSystemOfReactions ()
     {
-        //TODO
-
-        Reactor reactor = CreateReactor( "PushPull" );
-        //Reaction phosphorylationReaction = Resources.Load( "Tests/Reactions/pushPullCatalysisC" ) as Reaction;
-        //Reaction dephosphorylationReaction = Resources.Load( "Tests/Reactions/pushPullCatalysisD" ) as Reaction;
+        Reactor reactor = CreateReactor( "MultisitePhosphorylationE4" );
+        Reaction e1 = Resources.Load( "Tests/Reactions/multisitePCatalysisE1" ) as Reaction;
+        Reaction f1 = Resources.Load( "Tests/Reactions/multisitePCatalysisF1" ) as Reaction;
+        Reaction e2 = Resources.Load( "Tests/Reactions/multisitePCatalysisE2" ) as Reaction;
+        Reaction f2 = Resources.Load( "Tests/Reactions/multisitePCatalysisF2" ) as Reaction;
+        Reaction e3 = Resources.Load( "Tests/Reactions/multisitePCatalysisE3" ) as Reaction;
+        Reaction f3 = Resources.Load( "Tests/Reactions/multisitePCatalysisF3" ) as Reaction;
+        Reaction e4 = Resources.Load( "Tests/Reactions/multisitePCatalysisE4" ) as Reaction;
+        Reaction f4 = Resources.Load( "Tests/Reactions/multisitePCatalysisF4" ) as Reaction;
 
         yield return new WaitForEndOfFrame();
-
-        //BindingSite bindingSiteP = reactor.model.complexes[2].complexState.moleculeStates[0].molecule.bindingSites["p"];
-        //ReactionSimulator phosphorylationReactionSimulator = null, dephosphorylationReactionSimulator = null;
-        //foreach (CollisionFreeReactionSimulator reactionSimulator in reactor.collisionFreeReactionSimulators)
-        //{
-        //    if (reactionSimulator.reaction == phosphorylationReaction)
-        //    {
-        //        phosphorylationReactionSimulator = reactionSimulator;
-        //    }
-        //    else if (reactionSimulator.reaction == dephosphorylationReaction)
-        //    {
-        //        dephosphorylationReactionSimulator = reactionSimulator;
-        //    }
-        //}
+        Debug.Log( reactor.model.complexes[2].complexState.moleculeStates[0].molecule.name );
+        BindingSite bindingSiteP1 = reactor.model.complexes[2].complexState.moleculeStates[0].molecule.bindingSites["p1"];
+        BindingSite bindingSiteP2 = reactor.model.complexes[2].complexState.moleculeStates[0].molecule.bindingSites["p2"];
+        BindingSite bindingSiteP3 = reactor.model.complexes[2].complexState.moleculeStates[0].molecule.bindingSites["p3"];
+        BindingSite bindingSiteP4 = reactor.model.complexes[2].complexState.moleculeStates[0].molecule.bindingSites["p4"];
+        ReactionSimulator e1ReactionSimulator = null, f1ReactionSimulator = null, e2ReactionSimulator = null, f2ReactionSimulator = null,
+                          e3ReactionSimulator = null, f3ReactionSimulator = null, e4ReactionSimulator = null, f4ReactionSimulator = null;
+        foreach (CollisionFreeReactionSimulator reactionSimulator in reactor.collisionFreeReactionSimulators)
+        {
+            if (reactionSimulator.reaction == e1)
+            {
+                e1ReactionSimulator = reactionSimulator;
+            }
+            else if (reactionSimulator.reaction == f1)
+            {
+                f1ReactionSimulator = reactionSimulator;
+            }
+            else if (reactionSimulator.reaction == e2)
+            {
+                e2ReactionSimulator = reactionSimulator;
+            }
+            else if (reactionSimulator.reaction == f2)
+            {
+                f2ReactionSimulator = reactionSimulator;
+            }
+            else if (reactionSimulator.reaction == e3)
+            {
+                e3ReactionSimulator = reactionSimulator;
+            }
+            else if (reactionSimulator.reaction == f3)
+            {
+                f3ReactionSimulator = reactionSimulator;
+            }
+            else if (reactionSimulator.reaction == e4)
+            {
+                e4ReactionSimulator = reactionSimulator;
+            }
+            else if (reactionSimulator.reaction == f4)
+            {
+                f4ReactionSimulator = reactionSimulator;
+            }
+        }
 
         for (int i = 0; i < numberOfTimesToCheck; i++)
         {
             yield return new WaitForSeconds( waitTime );
             yield return new WaitForEndOfFrame();
 
-            //AssertIsTrue( GetNumberOfBindingSiteSimulatorsInState( bindingSiteP, "P" ) == phosphorylationReactionSimulator.events - dephosphorylationReactionSimulator.events );
-            AssertIsTrue( true );
+            AssertIsTrue( GetNumberOfBindingSiteSimulatorsInState( bindingSiteP1, "P" ) == e1ReactionSimulator.events - f1ReactionSimulator.events );
+            AssertIsTrue( GetNumberOfBindingSiteSimulatorsInState( bindingSiteP2, "P" ) == e2ReactionSimulator.events - f2ReactionSimulator.events );
+            AssertIsTrue( GetNumberOfBindingSiteSimulatorsInState( bindingSiteP3, "P" ) == e3ReactionSimulator.events - f3ReactionSimulator.events );
+            AssertIsTrue( GetNumberOfBindingSiteSimulatorsInState( bindingSiteP4, "P" ) == e4ReactionSimulator.events - f4ReactionSimulator.events );
         }
 
         DestroyReactor( reactor );
