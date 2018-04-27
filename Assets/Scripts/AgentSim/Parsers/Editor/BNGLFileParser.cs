66b496027556258dc80a78976d32c73af6a84934
Eric Isaac <erici@alleninstitute.org>
2018-04-27 09:58:12 -0700

    BNGL abstract math expression eval, handle line continuation (untested)

--- Assets/Scripts/AgentSim/Parsers/Editor/BNGLFileParser.cs
+++ Assets/Scripts/AgentSim/Parsers/Editor/BNGLFileParser.cs
@@ -15,12 +15,14 @@ namespace AICS.AgentSim
 
         private BNGLFileParser()
         {
+            symbols["parameters"] = new Func<List<string>, BNGLFileData, int>(ParseParameters);
             symbols["molecule types"] = new Func<List<string>, BNGLFileData, int>(ParseMoleculeTypes);
             symbols["seed species"] = new Func<List<string>, BNGLFileData, int>(ParseSeedSpecies);
-            symbols["parameters"] = new Func<List<string>, BNGLFileData, int>(ParseParameters);
             symbols["observables"] = new Func<List<string>, BNGLFileData, int>(ParseObservables);
             symbols["reaction rules"] = new Func<List<string>, BNGLFileData, int>(ParseReactionRules);
             symbols["actions"] = new Func<List<string>, BNGLFileData, int>(ParseActions);
+
+            //@TODO: What are species? They are not in the textbook spec
         }
 
         // The Directory where we expect to find BNGL files
@@ -65,6 +67,11 @@ namespace AICS.AgentSim
 
 
         #region BNGL Specific Parsing
+        /// <summary>
+        /// Parse a file's contents for keywords (words that signify the type of parsing needed)
+        /// e.g. "molecule type" -> Molecule specific parsing
+        /// </summary>
+        /// <param name="file_contents"> A string containing the contents of a local file read into memory</param>
         private void ParseForKeywords(string file_contents)
         {
             // Divide the string by new lines
@@ -118,7 +125,6 @@ namespace AICS.AgentSim
             // Divide the document into info-blocks and parse accordingly
             while (begin_indicies.Count > 0)
             {
-                //@TODO: Parse lines ending with continuation character '\' 
                 int begin = begin_indicies.Dequeue();
                 int end = end_indicies.Dequeue();
                 List<string> info = new List<string>();
@@ -137,6 +143,15 @@ namespace AICS.AgentSim
                     currentLine = currentLine.Trim();
                     if (string.IsNullOrEmpty(currentLine)) continue;
 
+                    // Check for the line continuation character
+                    while(currentLine.EndsWith(@"\") && j < (end - 1))
+                    {
+                        j++;
+
+                        // remove the continuation character and append the next line
+                        currentLine = currentLine.Substring(0, currentLine.Length - 1) + file_lines[j];
+                    }
+
                     // If the line isn't a comment line, empty, or null, add the line to the parseable info
                     info.Add(currentLine);
                 }
@@ -152,28 +167,14 @@ namespace AICS.AgentSim
             }
         }
 
-        private int ParseMoleculeTypes(List<string> info, BNGLFileData fdata)
-        {
-            // parse each molecule line-by-line
-            foreach(string s in info)
-            {
-                for(int i = 0; i < info.Count; ++i)
-                {
-                    string line = info[i];
-  
-                    MoleculeFileData md = ParseMolecule(line);
-                    fdata.molecules.Add(md);
-                }
-            }
-
-            return 0;
-        }
-
-        private int ParseReactionRules(List<string> info, BNGLFileData fdata)
-        {
-            return 0;
-        }
-
+        /// <summary>
+        /// Parses a list of file-lines, line by line, for parameter data
+        /// Generally, expected to be called first in file parsing
+        /// Parameter data is used in subsequent sections of the BNGL file
+        /// </summary>
+        /// <param name="info"> The strings (file lines) to parse for data</param>
+        /// <param name="fdata"> The BNGL File data object that holds parsed data</param>
+        /// <returns>0(int) for successfull completion</returns>
         private int ParseParameters(List<string> info, BNGLFileData fdata)
         {
             foreach (string p in info)
@@ -187,31 +188,142 @@ namespace AICS.AgentSim
                     return -1;
                 }
 
-                fdata.parameters.Add(p_arr[0], float.Parse(p_arr[1]));
+                float num = 0;
+                bool isNum = float.TryParse(p_arr[1], out num);
+
+                if (isNum) // a number was assigned as a parameter value
+                {
+                    fdata.parameters.Add(p_arr[0], num);
+                }
+                else // an equation (with parameters) was defined as a parameter value
+                {
+                    num = MathParser.Evaluate(p_arr[1], fdata.parameters);
+                    fdata.parameters.Add(p_arr[0], num);
+                }
             }
 
             return 0;
         }
 
-        private int ParseSeedSpecies(List<string> info, BNGLFileData fdata)
+        /// <summary>
+        /// Parses a list of file-lines, line by line, for molecule type data
+        /// Generally, expected to be called second in file parsing
+        /// Molecule types describe the allowed states of molecule binding-sites in the simulation
+        /// </summary>
+        /// <param name="info"> The strings (file lines) to parse for data</param>
+        /// <param name="fdata"> The BNGL File data object that holds parsed data</param>
+        /// <returns>0(int) for successfull completion</returns>
+        private int ParseMoleculeTypes(List<string> info, BNGLFileData fdata)
         {
+            // parse each molecule line-by-line
+            foreach (string s in info)
+            {
+                for (int i = 0; i < info.Count; ++i)
+                {
+                    string line = info[i];
+
+                    MoleculeFileData md = ParseMolecule(line);
+                    fdata.molecules.Add(md);
+                }
+            }
+
             return 0;
         }
 
+
+        /// <summary>
+        /// Parses a list of file-lines, line by line, for seed species data
+        /// Generally, expected to be called third in file parsing
+        /// Seed species are species present at the beginning of the simulation
+        /// </summary>
+        /// <param name="info"> The strings (file lines) to parse for data</param>
+        /// <param name="fdata"> The BNGL File data object that holds parsed data</param>
+        /// <returns>0(int) for successfull completion</returns>
+        private int ParseSeedSpecies(List<string> info, BNGLFileData fdata)
+        {
+            foreach(string s in info)
+            {
+                int rp = s.IndexOf(")"); // assuming the following layout:
+                                         // [molecule]"(" [binding-sites] ")" [numerical value]
+
+                MoleculeFileData md = ParseMolecule(s.Substring(0, rp + 1));
+                string iv_string = s.Substring(rp + 1).Trim();
+
+                // Check that a value was specified for the seed species
+                if(String.IsNullOrEmpty(iv_string))
+                {
+                    Debug.LogError(String.Format("{0} Seed Species is missing a value.\n{1}", fileErrorMsg, s));
+                    return -1;
+                }
+
+                SeedSpeciesFileData ssd = new SeedSpeciesFileData();
+                ssd.molecule = md;
+
+                float n = 0;
+                bool isNum = float.TryParse(iv_string, out n);
+
+                if(isNum) // A number was specified as the initial value for this seed species
+                {
+                    ssd.initialValue = n;
+                }
+                else // A parameter was specified as the initial value for this seed species
+                {
+                    // Check that the specified parameter was specified and parsed
+                    if(!fdata.parameters.ContainsKey(iv_string))
+                    {
+                        Debug.LogError(String.Format("{0} A Seed Species referenced an undefined parameter.\n{1}",fileErrorMsg,s));
+                        return -1;
+                    }
+
+                    ssd.initialValue = fdata.parameters[iv_string];
+                }
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Parses a list of file-lines, line by line, for observables data
+        /// Generally, expected to be called fourth in file parsing
+        /// Observables are quantities of interest that are measured (observed) at regular intervals
+        /// </summary>
+        /// <param name="info"> The strings (file lines) to parse for data</param>
+        /// <param name="fdata"> The BNGL File data object that holds parsed data</param>
+        /// <returns>0(int) for successfull completion</returns>
         private int ParseObservables(List<string> info, BNGLFileData fdata)
         {
             return 0;
         }
 
-        private int ParseActions(List<string> info, BNGLFileData fdata)
+        /// <summary>
+        /// Parses a list of file-lines, line by line, for reaction rules data
+        /// Generally, expected to be called fifth in file parsing
+        /// Reaction Rules are a text equation that describe how molecules can interact
+        /// </summary>
+        /// <param name="info"> The strings (file lines) to parse for data</param>
+        /// <param name="fdata"> The BNGL File data object that holds parsed data</param>
+        /// <returns>0(int) for successfull completion</returns>
+        private int ParseReactionRules(List<string> info, BNGLFileData fdata)
         {
             return 0;
+        }
+
+        // Unsure if needed at the moment (specifically for the unity simulation prototype)
+        private int ParseActions(List<string> info, BNGLFileData fdata)
+        {
+            Debug.LogWarning("Parsing actions is currently unimplemented");
+            return -1;
         }
         #endregion
 
         #region Private Data Type Parsing
         // All of the Private Data Type Parsing functions assume previous validation for null, empty, or comment lines
 
+        /// <summary>
+        /// Parse a file line into a molecule type data object, with a list of binding sites
+        /// </summary>
+        /// <param name="line"> The file line to parse for data</param>
+        /// <returns> The data constructed by parsing the file line</returns>
         private MoleculeFileData ParseMolecule(string line)
         {
             if(!line.Contains("(") || !line.Contains(")"))
@@ -243,6 +355,11 @@ namespace AICS.AgentSim
             return m;
         }
 
+        /// <summary>
+        /// Parse a string into a a binding site data object, with allowable states
+        /// </summary>
+        /// <param name="line"> The file line to parse for data</param>
+        /// <returns> The data constructed by parsing the file line</returns>
         private BindingSiteFileData ParseBindingSite(string line)
         {
             BindingSiteFileData b = new BindingSiteFileData();
@@ -273,22 +390,38 @@ namespace AICS.AgentSim
 
         #region Private Data Types
         // these data types are only intended for use in file IO, not for simulation
+
+        /// <summary>
+        /// A file IO internal data object containing information about the parsed BNGL file
+        /// </summary>
         private class BNGLFileData
         {
             public List<MoleculeFileData> molecules = new List<MoleculeFileData>();
             public Dictionary<string, float> parameters = new Dictionary<string, float>();
         }
 
+        /// <summary>
+        /// A file IO internal data object containing information about a parsed Moleculte Type
+        /// </summary>
         private class BindingSiteFileData
         {
             public string name = "";
             public List<string> allowableStates = new List<string>();
         }
 
+        /// <summary>
+        /// A file IO internal data object containing information about a parsed Binding Site
+        /// </summary>
         private class MoleculeFileData
         {
             public string name = "";
             public List<BindingSiteFileData> bindingSites = new List<BindingSiteFileData>();
+        }
+
+        private class SeedSpeciesFileData
+        {
+            public MoleculeFileData molecule = new MoleculeFileData();
+            public float initialValue = 0.0f;
         }
         #endregion
     }
---
+++ Assets/Scripts/AgentSim/Parsers/Editor/MathParser.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+namespace AICS.AgentSim
+{
+    /// <summary>
+    /// This class evaluates simple math expressions
+    /// </summary>
+    public static class MathParser
+    {
+        /// <summary>
+        /// Evaluate a simple mathematical expression
+        /// Recognizes +, -, *, /, and ()
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <param name="variables"></param>
+        /// <returns></returns>
+        public static float Evaluate(string expression, Dictionary<string,float> variables)
+        {
+            float sum = 0;
+            Debug.LogWarning("Math Expression Evaluation is currently unimplemented");
+            return sum;
+        }
+    }
+}
