f5c4dc3b8f1e5dba5ee57eca82dd4082fc8fde55
Blair Lyons <blair208@gmail.com>
2018-05-10 13:28:56 -0700

    basic click to focus, requires a Collider and a Focuser component on a Molecule visualization prefab

--- Assets/Scripts/AgentSim/Observer.cs
+++ Assets/Scripts/AgentSim/Observer.cs
@@ -8,5 +8,26 @@ namespace AICS.AgentSim
 	{
         [Tooltip( "meters" )]
 		public float currentScale = 1e-9f;
+
+        FollowLookZoomCamera _cameraController;
+        FollowLookZoomCamera cameraController
+        {
+            get
+            {
+                if (_cameraController == null)
+                {
+                    _cameraController = GetComponentInParent<FollowLookZoomCamera>();
+                }
+                return _cameraController;
+            }
+        }
+
+        public void FocusOn (Transform _target)
+        {
+            if (cameraController != null)
+            {
+                cameraController.target = _target;
+            }
+        }
 	}
 }
---
+++ Assets/Scripts/AgentSim/UI/Focuser.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AICS.AgentSim
+{
+    [RequireComponent( typeof(Collider) )]
+    public class Focuser : MonoBehaviour 
+    {
+		void OnMouseUp()
+		{
+            World.Instance.observer.FocusOn( transform );
+		}
+	}
+}
