43e0982c2e71ead3d39318644e4bf78442d865fa
Blair Lyons <blair208@gmail.com>
2018-05-10 16:21:49 -0700

    WIP selected molecule is outlined

---
+++ Assets/Scripts/AgentSim/UI/InputManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace AICS.AgentSim
+{
+    public class InputManager : MonoBehaviour 
+    {
+        static InputManager _Instance;
+        public static InputManager Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = GameObject.FindObjectOfType<InputManager>();
+                }
+                return _Instance;
+            }
+        }
+
+        public void SelectObject (GameObject obj)
+        {
+            EventSystem.current.SetSelectedGameObject( obj );
+            World.Instance.observer.FocusOn( obj.transform );
+        }
+    }
+}
---
+++ Assets/Scripts/AgentSim/UI/Selecter.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using cakeslice;
+
+namespace AICS.AgentSim
+{
+    [RequireComponent( typeof(Collider), typeof(Outline) )]
+    public class Selecter : MonoBehaviour, ISelectHandler, IDeselectHandler
+    {
+        Outline _outline;
+        Outline outline
+        {
+            get
+            {
+                if (_outline == null)
+                {
+                    _outline = GetComponentInChildren<Outline>( true );
+                }
+                return _outline;
+            }
+        }
+
+        void OnMouseEnter ()
+        {
+            if (EventSystem.current.currentSelectedGameObject != gameObject)
+            {
+                outline.enabled = true;
+            }
+        }
+
+        void OnMouseExit ()
+        {
+            if (EventSystem.current.currentSelectedGameObject != gameObject)
+            {
+                outline.enabled = false;
+            }
+        }
+
+		void OnMouseUp()
+		{
+            InputManager.Instance.SelectObject( gameObject );
+		}
+
+        public void OnSelect (BaseEventData eventData)
+        {
+            outline.enabled = true;
+            outline.color = 1;
+        }
+
+        public void OnDeselect (BaseEventData eventData)
+        {
+            outline.enabled = false;
+            outline.color = 0;
+        }
+	}
+}
