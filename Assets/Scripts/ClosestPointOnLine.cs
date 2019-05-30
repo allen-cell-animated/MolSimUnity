c2d51ed3f776f5e533bf0677ea8485507cd45c3a
Blair Lyons <blairl@alleninstitute.org>
2019-05-30 16:42:12 -0700

    re-constructed actin using rotation axes calculated in chimera

---
+++ Assets/Scripts/ClosestPointOnLine.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClosestPointOnLine : MonoBehaviour 
+{
+	void Start () 
+    {
+		
+	}
+
+    public Vector3 GetNearestPointOnLine (Vector3 lineStart, Vector3 lineEnd, Vector3 point)
+    {
+        Vector3 lineDir = (lineEnd - lineStart).normalized;
+        Vector3 v = point - lineStart;
+        float d = Vector3.Dot(v, lineDir);
+        return lineStart + lineDir * d;
+    }
+}
